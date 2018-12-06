08f3787047d62c6b174ee6e0fa9f54e39a171112
Clément Foubert <clement.froz@gmail.com>
2018-12-07 00:28:33 +0100

    Création DatabaseContext *A faire : Vérifier connexion

---
+++ Model1/Kitchen/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Kitchen.DAL;
+
+namespace Model.Kitchen
+{
+        class DatabaseContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"data source=tcp:DESKTOP-NTF3CC3,1433;initial catalog=BDDRestaurant;persist security info=True;Integrated Security=SSPI; user id = sa; password = root;");
+            }
+            public DbSet<CategoryDao> Category { get; set; }
+            public DbSet<ProductDao> Product { get; set; }
+            public DbSet<StorageDao> Storage { get; set; }
+
+        }
+
+}
