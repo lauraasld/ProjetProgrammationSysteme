1b649a57e0bf1370950e0fff6bce4d997b104d3e
C-Benjamin <benjamin.cuny@viacesi.fr>
2018-12-11 19:28:10 +0100

    ajout dépendances entre projets

--- Model2/PositionedElement.cs
+++ Model2/PositionedElement.cs
@@ -5,7 +5,7 @@ namespace Model
 {
     public class PositionedElement
     {
-        public Point Point { get; set; }
+        //public Point Point { get; set; }
         public ElementType ElementType { get; set; }
     }
 }
