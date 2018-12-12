f29ba9f242c825968b01163c8af6f1fffe29eb5f
Clément Foubert <clement.froz@gmail.com>
2018-12-12 01:14:02 +0100

    Servefood() dans serveur

--- Model2/DiningRoom/Waiter.cs
+++ Model2/DiningRoom/Waiter.cs
@@ -1,24 +1,53 @@
 using System; using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace Model.DiningRoom {
 	public class Waiter : DiningRoomStaff , IPlatesToServeObserver  {
         private double timeMultiplier = 1;
+        public Kitchen.Countertop plateToServe;
+
         public Waiter(DiningRoom diningRoom) : base(diningRoom)
         {            
-            this.timeMultiplier = timeMultiplier;
+            this.timeMultiplier = 1;
         }
         public void GiveBreadAndWater(int tableNumber) {
         }
+
 		public void ServeFood(int tableNumber) {
+            
             foreach (var item in diningRoom.Tables)
             {
-                if (item.TableNumber == tableNumber)
-                {
+                List<Plate> pTS = plateToServe.PlatesToServe;
+                List<TableOrder> orders = plateToServe.Orders;
 
+                if (item.TableNumber == tableNumber)
+                { 
+                    foreach (var order in orders)
+                    {
+                        List<Dish> ordersDish = order.Orders.Values.ToList();
+                        foreach(var orderDish in ordersDish)
+                        {
+                            foreach (var plate in pTS)
+                            {
+                                if (orderDish.DishName.Equals(plate.Name) /*&& orderDish.CourseType.Equals(plate.courseType)*/)
+                                {
+                                    pTS.Remove(plate);
+                                }
+                            }
+                        }
+                        //Passer au CourseType suivant
+                        if (order.NextCourseToServe == CourseType.Starter)
+                        {
+                            order.NextCourseToServe = CourseType.MainCourse;
+                        }
+                        else if (order.NextCourseToServe == CourseType.MainCourse)
+                        {
+                            order.NextCourseToServe = CourseType.Dessert;
+                        }
+                    }
                 }
             }
-            //récupère la liste table order
             // ne prend en compte que CourseType
             // les Comparer a la liste PlateToServe
             //Si ok -> remove de la liste PlateToServe + CourseType+=1
