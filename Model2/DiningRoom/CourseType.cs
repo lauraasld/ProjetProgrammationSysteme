205d33e8c99f4b34b65669d24f8354b397008065
Clément Foubert <clement.froz@gmail.com>
2018-12-12 14:31:43 +0100

    changements table et ajouts waiter

--- Model2/DiningRoom/CourseType.cs
+++ Model2/DiningRoom/CourseType.cs
@@ -1,7 +1,7 @@
 using System; using System.Collections.Generic;
 namespace Model.DiningRoom {
 	public enum CourseType {
-        Starter, MainCourse, Dessert
+        Starter = 1, MainCourse = 2, Dessert = 3, None = 4
 	}
 
 }
--- Model2/DiningRoom/Waiter.cs
+++ Model2/DiningRoom/Waiter.cs
@@ -4,89 +4,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
-namespace Model.DiningRoom {
-	public class Waiter : DiningRoomStaff , IPlatesToServeObserver  {
+namespace Model.DiningRoom
+{
+    public class Waiter : DiningRoomStaff, IPlatesToServeObserver
+    {
         private double timeMultiplier = 1;
         public Waiter(DiningRoom diningRoom) : base(diningRoom)
-        {            
+        {
             this.timeMultiplier = 1;
         }
-        public void GiveBreadAndWater(int tableNumber) {
+        public void GiveBreadAndWater(int tableNumber)
+        {
         }
 
-		public void ServeFood(int tableNumber) {
-            //faire un linq
-            foreach (var diningRoomTable in diningRoom.Tables)
-            {
-                List<Plate> pTS = diningRoom.Countertop.PlatesToServe;
-                List<TableOrder> orders = diningRoom.Countertop.Orders;
-               
-                if (diningRoomTable.TableNumber == tableNumber)
-                {
-                    //var test = from p in pTS
-                    //           join o in orders on p.Dish.DishName equals o.Orders.Where(x => x.Value.DishName == p.Dish)
-                    //           select p;
+        public void ServeFood(int tableNumber)
+        {
+            Table tableToServe = diningRoom.Tables.Find(t => t.TableNumber == tableNumber);
+            List<Plate> platesReadyToServe = diningRoom.Countertop.PlatesToServe;
 
-                    //foreach (var plate in pTS)
-                    //{
-                    //    diningRoom.Countertop.Orders.First(x => x.Orders.Values.ToList()
-                    //    .ForEach(i => { if (i.DishName.Equals(plate.Dish.DishName) && x.CourseType.Equals(plate.Dish.CourseType)) { pTS.Remove(plate); } }));
-                    //}
-                    foreach (var order in orders)
+            //foreach (var plate in pTS)
+            //{
+            //    diningRoom.Countertop.Orders.First(x => x.Orders.Values.ToList()
+            //    .ForEach(i => { if (i.DishName.Equals(plate.Dish.DishName) && x.CourseType.Equals(plate.Dish.CourseType)) { pTS.Remove(plate); } }));
+            //}
+            foreach (var order in tableToServe.OrderedDishes.Where(x => x.CourseType == tableToServe.NextCourseToServe))
+            {
+                foreach (var plat in platesReadyToServe)
+                {
+                    if (order.DishName == plat.Dish.DishName)
                     {
-                        List<Dish> ordersDish = order.Orders.Values.ToList();
-                        foreach (var oDish in ordersDish)
-                        {
-                            foreach (var plat in pTS)
-                            {
-                                if (oDish.DishName.Equals(plat.Dish.DishName) && oDish.CourseType.Equals(plat.Dish.CourseType))
-                                {
-                                    foreach (var place in diningRoomTable.Places)
-                                    {
-                                        place.CookedFood = plat;
-                                    }
-                                    pTS.Remove(plat);
-                                }
-                            }
-                        }
-                        //Passer au CourseType suivant
-                        if (order.NextCourseToServe == CourseType.Starter)
-                        {
-                            order.NextCourseToServe = CourseType.MainCourse;
-                        }
-                        else if (order.NextCourseToServe == CourseType.MainCourse)
-                        {
-                            order.NextCourseToServe = CourseType.Dessert;
-                        }
+                        tableToServe.ServedFood.Add(plat);
+                        platesReadyToServe.Remove(plat);
+                        break;
                     }
                 }
             }
-            // ne prend en compte que CourseType
-            // les Comparer a la liste PlateToServe
-            //Si ok -> remove de la liste PlateToServe + CourseType+=1
+            tableToServe.NextCourseToServe++;
+            //Passer au CourseType suivant
         }
-		public void ClearPlates(int tableNumber) {
-            foreach (var diningRoomTable in diningRoom.Tables)
-            {
-                if (diningRoomTable.TableNumber == tableNumber)
-                {
-                    foreach (var place in diningRoomTable.Places)
-                    {
-                        if (place.CookedFood.IsFinished)
-                        {
-                            diningRoom.Countertop.ItemsGoingToKitchen.Add(place.CookedFood);
-                            place.CookedFood = null;
-                        }
-                    }
+        // ne prend en compte que CourseType
+        // les Comparer a la liste PlateToServe
+        //Si ok -> remove de la liste PlateToServe + CourseType+=1
 
-                }
+        public void ClearPlates(int tableNumber)
+        {
+            Table tableToClear = diningRoom.Tables.Find(t => t.TableNumber == tableNumber);
+
+            if (!tableToClear.ServedFood.Any(x => x.IsFinished == false))
+            {
+                diningRoom.Countertop.ItemsGoingToKitchen.AddRange(tableToClear.ServedFood);
+                tableToClear.ServedFood.Clear();
             }
             //quand plat fini, alors débarasse IsReadyToServe == true
         }
-		public void ClearAndCleanTable(int tableNumber) {
-			throw new Exception("Not implemented");
-		}
-		public void NewPlateIsReady() {
+        public void ClearAndCleanTable(int tableNumber)
+        {
+            diningRoom.Tables.Find(t => t.TableNumber == tableNumber).IsAvailable = true;
+        }
+        public void NewPlateIsReady()
+        {
+            // tester sitous les plats sont prets pour une des table
+            // si oui, appelle ServeFood
             throw new Exception("Not implemented");
         }
 
