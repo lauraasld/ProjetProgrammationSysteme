e29a27cb9e0d4350c2970a3dac1c29edf7b6d819
Clément Foubert <clement.froz@gmail.com>
2018-12-12 13:28:46 +0100

    je push psk laura elle va tout casser

--- Model2/DiningRoom/Waiter.cs
+++ Model2/DiningRoom/Waiter.cs
@@ -1,12 +1,12 @@
-using System; using System.Collections.Generic;
+using Model.Kitchen;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
 namespace Model.DiningRoom {
 	public class Waiter : DiningRoomStaff , IPlatesToServeObserver  {
         private double timeMultiplier = 1;
-        public Kitchen.Countertop plateToServe;
-
         public Waiter(DiningRoom diningRoom) : base(diningRoom)
         {            
             this.timeMultiplier = 1;
@@ -15,24 +15,37 @@ namespace Model.DiningRoom {
         }
 
 		public void ServeFood(int tableNumber) {
-            
-            foreach (var item in diningRoom.Tables)
+            //faire un linq
+            foreach (var diningRoomTable in diningRoom.Tables)
             {
-                List<Plate> pTS = plateToServe.PlatesToServe;
-                List<TableOrder> orders = plateToServe.Orders;
+                List<Plate> pTS = diningRoom.Countertop.PlatesToServe;
+                List<TableOrder> orders = diningRoom.Countertop.Orders;
+               
+                if (diningRoomTable.TableNumber == tableNumber)
+                {
+                    //var test = from p in pTS
+                    //           join o in orders on p.Dish.DishName equals o.Orders.Where(x => x.Value.DishName == p.Dish)
+                    //           select p;
 
-                if (item.TableNumber == tableNumber)
-                { 
+                    //foreach (var plate in pTS)
+                    //{
+                    //    diningRoom.Countertop.Orders.First(x => x.Orders.Values.ToList()
+                    //    .ForEach(i => { if (i.DishName.Equals(plate.Dish.DishName) && x.CourseType.Equals(plate.Dish.CourseType)) { pTS.Remove(plate); } }));
+                    //}
                     foreach (var order in orders)
                     {
                         List<Dish> ordersDish = order.Orders.Values.ToList();
-                        foreach(var orderDish in ordersDish)
+                        foreach (var oDish in ordersDish)
                         {
-                            foreach (var plate in pTS)
+                            foreach (var plat in pTS)
                             {
-                                if (orderDish.DishName.Equals(plate.Name) /*&& orderDish.CourseType.Equals(plate.courseType)*/)
+                                if (oDish.DishName.Equals(plat.Dish.DishName) && oDish.CourseType.Equals(plat.Dish.CourseType))
                                 {
-                                    pTS.Remove(plate);
+                                    foreach (var place in diningRoomTable.Places)
+                                    {
+                                        place.CookedFood = plat;
+                                    }
+                                    pTS.Remove(plat);
                                 }
                             }
                         }
@@ -53,15 +66,30 @@ namespace Model.DiningRoom {
             //Si ok -> remove de la liste PlateToServe + CourseType+=1
         }
 		public void ClearPlates(int tableNumber) {
-			//quand plat fini, alors débarasse IsReadyToServe == true
-		}
+            foreach (var diningRoomTable in diningRoom.Tables)
+            {
+                if (diningRoomTable.TableNumber == tableNumber)
+                {
+                    foreach (var place in diningRoomTable.Places)
+                    {
+                        if (place.CookedFood.IsFinished)
+                        {
+                            diningRoom.Countertop.ItemsGoingToKitchen.Add(place.CookedFood);
+                            place.CookedFood = null;
+                        }
+                    }
+
+                }
+            }
+            //quand plat fini, alors débarasse IsReadyToServe == true
+        }
 		public void ClearAndCleanTable(int tableNumber) {
 			throw new Exception("Not implemented");
 		}
 		public void NewPlateIsReady() {
-			throw new Exception("Not implemented");
-		}
+            throw new Exception("Not implemented");
+        }
 
-	}
+    }
 
 }
