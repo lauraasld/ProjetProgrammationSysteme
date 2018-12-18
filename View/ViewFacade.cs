1c4ed7e6d9626e27b20859b18bd2ce3d42b0420c
C-Benjamin <benjamin.cuny@viacesi.fr>
2018-12-18 01:37:18 +0100

    nettoyage (supp code commenté/classe inutilisée)

--- View/ViewFacade.cs
+++ View/ViewFacade.cs
@@ -6,6 +6,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.Timers;
 using System;
+using System.Windows;
 
 namespace View
 {
@@ -37,61 +38,12 @@ namespace View
 
         public void Start()
         {
-            //timer.Start();
+
         }
 
-        /*public void initializeLists()
-        {
-            int i = 0;
-            foreach (var Waiter in Model.DiningRoom.Waiters)
-            {
-                i++;
-                MainWindow.StaffElements.Add(new StaffElement("Serveur N°" + i, Waiter.IsBusy, Waiter.Action));
-            }
-            i = 0;
-            foreach (var HeadWaiter in Model.DiningRoom.HeadWaiters)
-            {
-                i++;
-                MainWindow.StaffElements.Add(new StaffElement("Chef de Rang N°" + i, HeadWaiter.IsBusy, HeadWaiter.Action));
-            }
-            MainWindow.StaffElements.Add(new StaffElement("Maître d'hotel", Model.DiningRoom.Butler.IsBusy, Model.DiningRoom.Butler.Action));
-            i = 0;
-            foreach (var Dishwasher in Model.Kitchen.Dishwashers)
-            {
-                i++;
-                MainWindow.StaffElements.Add(new StaffElement("PlongeurN°" + i, Dishwasher.IsBusy, Dishwasher.Action));
-            }
-            i = 0;
-            foreach (var Commis in Model.Kitchen.Commis)
-            {
-                i++;
-                MainWindow.StaffElements.Add(new StaffElement("Commis N°" + i, Commis.IsBusy, Commis.Action));
-            }
-            i = 0;
-            foreach (var Cook in Model.Kitchen.Cooks)
-            {
-                i++;
-                MainWindow.StaffElements.Add(new StaffElement("Cuisinier N°" + i, Cook.IsBusy, Cook.Action));
-            }
-            MainWindow.StaffElements.Add(new StaffElement("Chef de cuisine", Model.Kitchen.HeadChef.IsBusy, Model.Kitchen.HeadChef.Action));
-            foreach (var Table in Model.DiningRoom.Tables)
-            {
-                MainWindow.DiningTables.Add(new DiningTable("Table N°" + Table.TableNumber, Table.NumberOfPlaces, Table.NumberOfSeatedCustomers));
-            }
-        }*/
-        public void RefreshLists(object sender, ElapsedEventArgs e)
-        {
-            /*MainWindow.DiningTables = null;
-            MainWindow.StaffElements = null;*/
-            //initializeLists();
-            /*MainWindow.DiningTableGrid.ItemsSource = null;
-            MainWindow.DiningTableGrid.ItemsSource = diningTables;
-            MainWindow.StaffGrid.ItemsSource = null;
-            MainWindow.StaffGrid.ItemsSource = staffElements;*/
-        }
         public void DisplayMessage(string message)
         {
-            throw new System.Exception("Not implemented");
+            MessageBox.Show(message);
         }
         /*public void RefreshPersonPosition(PositionedElement person)
         {
